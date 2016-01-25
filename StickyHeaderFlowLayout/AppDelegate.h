5001521b6a78d569d2d8482b721f2e16926166b0
lingdaiping <lingdaiping@fangdd.com>
2016-01-25 16:42:13 +0800

    Initial Commit

---
+++ StickyHeaderFlowLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StickyHeaderFlowLayout
+//
+//  Created by lingyohunl on 16/1/25.
+//  Copyright © 2016年 yohunl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StickyHeaderFlowLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StickyHeaderFlowLayout
+//
+//  Created by lingyohunl on 16/1/25.
+//  Copyright © 2016年 yohunl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
